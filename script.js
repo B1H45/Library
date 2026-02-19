d5e2f176cb8d06cec0ca981b599f226d4804ca70
Ben Hustler <ben1hustler@gmail.com>
2026-02-19 08:27:49 +0000

    Finished all styling, only cacheing remains

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 // Book Functions
 
@@ -8,6 +8,30 @@ function Book(title, author, pgCount) {
     this.title = title;
     this.author = author;
     this.pgCount = pgCount;
+    this.read = false;
+}
+
+Book.prototype.toggleRead = function(readToggle) {
+  if (this.read==true) {
+    this.read = false;
+    readToggle.classList.remove("status-read");
+    readToggle.textContent = "Unread"
+  } else {
+    this.read = true;
+    readToggle.classList.add("status-read");
+    setTimeout(() => {
+      readToggle.textContent = "Read";
+    }, 100)
+  }
+  readToggle.classList.add("cooldown");
+  setTimeout(() => {
+    window.addEventListener("mousemove", () => {
+        readToggle.classList.remove("cooldown");
+    }, { once: true });
+    window.addEventListener("focusout", () => {
+        readToggle.classList.remove("cooldown");
+    }, { once: true });
+  }, 500);
 }
 
 function addBookToLibrary(title, author, pgCount) {
@@ -37,24 +61,57 @@ function displayBooks(myLibrary) {
         let titleTxt = document.createElement("h2");
         titleTxt.textContent = book.title;
         let authorTxt = document.createElement("p");
-        authorTxt.textContent = book.author;
+        authorTxt.textContent = "Author: " + book.author;
         let pgCountTxt = document.createElement("p");
-        pgCountTxt.textContent = book.pgCount;
+        pgCountTxt.textContent = "Pages: " + book.pgCount;
+
+        let buttonDiv = document.createElement("div");
+        buttonDiv.classList.add("button-div");
+
         let remButton = document.createElement("button");
         remButton.textContent = "Remove";
-        remButton.classList.add("rem-button");
-        
+        remButton.classList.add("invert");
+        remButton.classList.add("button");
         remButton.addEventListener("click", () => {
           remBookFromLibrary(book);
         })
 
+        let readToggle = document.createElement("button");
+        readToggle.classList.add("button");
+        readToggle.textContent = "Unread";
+        readToggle.classList.add("read-toggle");
+        if (book.read) {
+          readToggle.classList.add("status-read");
+          readToggle.textContent = "Unread";
+        }
+        readToggle.addEventListener("click", ()=>{
+          book.toggleRead(readToggle);
+        })
 
+        buttonDiv.appendChild(remButton);
+        buttonDiv.appendChild(readToggle);
+        
         cardInner.appendChild(titleTxt);
         cardInner.appendChild(authorTxt);
         cardInner.appendChild(pgCountTxt);
-        cardInner.appendChild(remButton);
+        cardInner.appendChild(buttonDiv);
 
         bookTable.appendChild(card);
+
+        // Read toggle implementation
+        // let node;
+        // const walker = document.createTreeWalker(
+        //     readToggle,
+        //     NodeFilter.SHOW_TEXT,
+        //     null
+        // );
+        // node = walker.nextNode();
+        // let range = document.createRange();
+        // range.selectNodeContents(node);
+        // let rect = range.getBoundingClientRect();
+        // console.log(rect.width + "!!!");
+        readToggle.style.width = readToggle.getBoundingClientRect().width + "px";
+
     }
 }
 
@@ -72,15 +129,16 @@ bookDialog.addEventListener("close", (e) => {
     displayBooks(myLibrary); 
   }
   bookForm.reset();
+  document.activeElement.blur(); // blur before closing
 });
 
 window.addEventListener("load", () => {
   bookForm.reset();
 })
 
-addBookToLibrary("Thing", "Dude", 100);
-addBookToLibrary("Thing", "Dude", 100);
-addBookToLibrary("Thing", "Dude", 100);
-addBookToLibrary("Thing", "Dude", 100);
+addBookToLibrary("The Restaurant at the End of the Universe", "Douglas Adams", 1231);
+addBookToLibrary("The Fellowship of the Ring", "J. R. R. Tolkien", 4123);
+addBookToLibrary("The Lion, the Witch, and the Wardrobe", "C. S. Lewis", 2322);
+addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1532);
 
 displayBooks(myLibrary);
